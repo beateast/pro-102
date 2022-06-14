156e9855b95e4bb0bec2cb134c14bb32d60521db
beateast <89135743+beateast@users.noreply.github.com>
2022-06-14 18:28:45 +0530

    Add files via upload

---
+++ first.py
@@ -0,0 +1,24 @@
+def menue():
+
+    while(1):
+        print("1. odd number")
+        print("2. even number")
+        no = input("Enter the num ")
+    
+    
+    
+
+        if(no == 1):
+            odd = input("Enter any number")
+
+        if odd % 2 == 0 :
+            print("It is not an odd number")
+
+        if no == 2:
+            even = input("Enter your number")
+
+        if even % 2 == 0:
+            print("It is an Even number")
+menue()
+        
+            
